c2a7d88d3c598c742aa57d0975227418e72bf26e
Yader Campbell <yadermejiac123@gmail.com>
2021-04-25 08:49:48 -0600

    JS funcion que recoje los datos del click

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Funcion que recoje los datos del button
+function getContentClick(event){
+    const value = event.target.innerHTML;
+    filterAction(value);
+}
+
+//Funcion que filtra los metodos
+const filterAction = value =>{
+
+}

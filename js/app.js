ad7046ec83dabd53fa85d900b4479c5a7ef9a67b
Yader Campbell <yadermejiac123@gmail.com>
2021-04-25 12:01:50 -0600

    JS añadimos metodo de guardar la operacion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var operation = null;
+var inputValueMemo = null;
+
 //Funcion que recoje los datos del button
 function getContentClick(event){
     const value = event.target.innerHTML;
@@ -17,6 +20,13 @@ const filterAction = value =>{
     value === "8" ? addNumberInput(8) : null;
     value === "9" ? addNumberInput(9) : null;
     value === "," ? addNumberInput(',') : null;
+
+    value === "+" ? setOperation('+') : null;
+    value === "-" ? setOperation('-') : null;
+    value === "X" ? setOperation('*') : null;
+    value === "/" ? setOperation('/') : null;
+    value === "%" ? setOperation('%') : null;
+    value === "+/-" ? setOperation('') : null;
 }
 
 //Funcion que añadea los valores al input o pantalla de la calculadora
@@ -32,4 +42,30 @@ function addNumberInput(value){
 
     //Concatenamos los valores que vamor ingresando
     inputScreen.value = inputValue + value;
+}
+
+//Funcion que recibe el operador y la guarda
+function setOperation(operation){
+    const inputScreen = document.getElementsByClassName("calculator__screen")[0].value;
+    this.operation = operation;
+
+    if(inputScreen != 0){
+        calculation();
+    }
+}
+
+//Funcion que guarda los calculas ya registrado
+function calculation(){
+    const inputScreen = document.getElementsByClassName("calculator__screen")[0];
+    let valueOne = this.inputValueMemo;
+    let valueTwo = inputScreen.value;
+    let total = 0;
+
+    if(this.operation === "+" && inputScreen.value != ""){
+        total = valueOne + valueTwo;
+    }
+
+    this.inputValueMemo = total;
+    this.inputScreen.value = "";
+    inputScreen.placeholder = total;
 }
